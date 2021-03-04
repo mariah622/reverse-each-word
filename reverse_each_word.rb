1e167e658379cc0e56bf86e6717f377e743b4d0b
Mariah Morales <mjustine622@gmail.com>
2021-03-03 20:15:10 -0500

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,8 @@
+def reverse_each_word(sentence)
+    words = sentence.split(" ")
+    reverse_sentence  =[]
+    words.collect do |word|
+        reverse_sentence << word.reverse
+    end 
+    reverse_sentence.join(" ")
+end 
